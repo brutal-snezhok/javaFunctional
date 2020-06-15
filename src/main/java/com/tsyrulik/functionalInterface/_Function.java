edf2b74a033a4b4619a5390f2baef8ed7673d6b1
brutal-snezhok <czyrulik97@mail.ru>
2020-06-15 13:00:42 +0300

    added combination of two functions

--- src/main/java/com/tsyrulik/functionalInterface/_Function.java
+++ src/main/java/com/tsyrulik/functionalInterface/_Function.java
@@ -9,7 +9,13 @@ public class _Function {
 
         Integer increment2 = incrementByOneFunction.apply(1);
         System.out.println(increment2);
+
+        Function<Integer, Integer> addBy1AndThenMultiplyBy10 = incrementByOneFunction.andThen(multipleBy10Function);
+
+        System.out.println(addBy1AndThenMultiplyBy10.apply(2));
     }
+
+    private static Function<Integer, Integer> multipleBy10Function = number -> number * 10;
 
     private static Function<Integer, Integer> incrementByOneFunction =
             number -> number + 1;

de925bb9663c6e67e6a153be6a367ff6dd019dc3
brutal-snezhok <czyrulik97@mail.ru>
2020-06-15 16:07:47 +0300

    added callback like in js

---
+++ src/main/java/com/tsyrulik/finalSection/Main.java
@@ -0,0 +1,30 @@
+package com.tsyrulik.finalSection;
+
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+
+        hello("John", "Montana", value -> System.out.println("no lastName provided for " + value));
+        System.out.println("*************");
+        hello("John", null, value -> System.out.println("no lastName provided for " + value));
+    }
+
+    static void hello(String firstName, String lastName, Consumer<String> callback) {
+        System.out.println(firstName);
+        if(lastName != null) {
+            System.out.println(lastName);
+        } else {
+            callback.accept(firstName);
+        }
+    }
+
+//    function hello(firstName, lastName, callback) {
+//        console.log(firstName);
+//        if(lastName) {
+//            console.log(lastName);
+//        } else {
+//            callback();
+//        }
+//    }
+}
